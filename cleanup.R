4dfbc53d7a58541a1b661c36c1d9f2de17af9654
chrbarua <90236578+chrbarua@users.noreply.github.com>
2022-07-25 19:50:09 -0500

    Update cleanup.R

--- cleanup.R
+++ cleanup.R
@@ -15,17 +15,21 @@ colnames(HP_data) <- HP_data[1, ]
 HP_data <- HP_data[- 1, ]
 
 HP_data[HP_data == "?"] <- NA
-#str(HP_data)
 
+str(HP_data)
+
+HP_data$Time <- strptime(paste(HP_data$Date, HP_data$Time), format = "%d/%m/%Y %H:%M:%S")
 HP_data$Date <- as.Date(HP_data$Date, format = "%d/%m/%Y")
-HP_data$Date <- format(HP_data$Date, format = "%Y/%m/%d")
-HP_data$Time <- strptime(HP_data$Time, format = "%H:%M:%S")
-HP_data$Time <- format(HP_data$Time, format = "%H:%M:%S")
-# sapply(HP_data$Date, class)
+
+str(HP_data)
+
+i <- c(3, 4, 5, 6, 7, 8, 9)
+
+HP_data[ , i] <- apply(HP_data[ , i], 2, function(x) as.numeric(as.character(x)))
+
+str(HP_data)
 
 View(HP_data)
-
-#write.csv(HP_data, "data/HP_data.txt", row.names = FALSE)
 
 SHP_data <- HP_data[HP_data$Date >= "2007/02/01" & HP_data$Date <="2007/02/02", ]
 View(SHP_data)
